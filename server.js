4964d1d07299f9dd5351febeecb217c4356814c5
Elis Jones <ej14433@my.bristol.ac.uk>
2018-07-09 11:49:59 +0100

    First heroku push

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const items = require('./routes/api/items');
@@ -20,7 +21,16 @@ mongoose.connect(db, { useNewUrlParser: true })
 // Use Routes
 app.use('/api/items', items);
 
-// Listem on port 5000
+// Serve static assets for production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static(express.static('client/build')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+// Listen on port 5000
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
