6e4acc1afe1942a401fdf46919cc4080ed28338e
Elis Jones <ej14433@my.bristol.ac.uk>
2018-07-09 12:25:43 +0100

    First heroku push

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api/items', items);
 // Serve static assets for production
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
-	app.use(express.static(express.static('client/build')));
+	app.use(express.static(express.static('./client/build')));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
